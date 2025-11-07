b66a4f7b437c1b1fe266ec9759f639cb377c7dd6
kalyan <kaltompe123@gmail.com>
2025-11-07 13:32:20 +0530

    terragrunt & opentofu

---
+++ day-14 -terrgrunt wrapper/main.tf
@@ -0,0 +1,2 @@
+##create ec2 instance & install terrgrunt , terraform , git .
+#attache IAM role &  ##git clone  httpsith://gub.com/CloudTechDevOps/Terragrunt-lab.git
---
+++ day-15 -(opentofu)/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+}
+
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "opentofu/aws"
+#       version = ">= 5.0"
+#     }
+#   }
+# }
