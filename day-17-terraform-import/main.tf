eb9c9f92aa9b19569c985f02e2f0b60d7c20ad1f
kalyan <kaltompe123@gmail.com>
2026-03-14 00:29:10 +0530

    import practice

---
+++ day-17-terraform-import/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "testname" {
+  ami           = "ami-00ca32bbc84273381"
+  instance_type = "t3.nano"
+}
+
+resource "aws_instance" "testname1" {
+  ami           = "ami-02457590d33d576c3"
+  instance_type = "t2.micro"
+}
